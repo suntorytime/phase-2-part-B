9aeeb25aed22641a6d4b177eb8e3598cca47c605
Torey Hickman <torey@toreyhickman.com>
2015-04-06 16:59:01 -0400

    Add Tests for AJAX
    Tweak the views to assist in testing.

--- part-1/spec/features/writing_a_post_spec.rb
+++ part-1/spec/features/writing_a_post_spec.rb
@@ -27,4 +27,40 @@ feature "writing a new post" do
       expect(page).to have_content "I've been doing a lot of thinking ..."
     end
   end
+
+  context "with javascript" do
+    scenario "user navigates to post form, submits it, and sees the post", { js: true } do
+      visit "/"
+        expect(page).to have_content "Share Anything"
+        expect(page).to have_content @previous_post.title
+
+      click_link("Share Your Thoughts")
+        # Link goes away
+        expect(page).to_not have_content "Share Your Thoughts"
+        # Don't render the posts/new template
+        expect(page).to_not have_content "Write Your Post"
+
+
+      within("#post_form") do
+        fill_in "post[title]", with: "Bright Idea"
+        fill_in "post[author_name]", with: "Evelyn"
+        fill_in "post[body]", with: "I've been doing a lot of thinking ..."
+      end
+
+      find("input[name='submit']").click
+        # Submitting the form stays on the page
+        expect(page).to have_content @previous_post.title
+        # Don't render the posts/show template
+        expect(page).to_not have_content "The Post ..."
+
+      # New post is added to the DOM
+      expect(page).to have_content "Bright Idea"
+      expect(page).to have_content "Evelyn"
+      expect(page).to have_content "I've been doing a lot of thinking ..."
+
+      # Remove form and show link again
+      expect(page).to_not have_selector("#post_form")
+      expect(page).to have_content "Share Your Thoughts"
+    end
+  end
 end
