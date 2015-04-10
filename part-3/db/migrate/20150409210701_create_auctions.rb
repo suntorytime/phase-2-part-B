cbf4498acd736f8a1b7e67e562f41c99a777733a
Torey Hickman <torey@toreyhickman.com>
2015-04-09 21:22:57 -0400

    Auction migration

--- part-3/db/migrate/20150409210701_create_auctions.rb
+++ part-3/db/migrate/20150409210701_create_auctions.rb
@@ -4,7 +4,7 @@ class CreateAuctions < ActiveRecord::Migration
       t.datetime :starts_at, { null: false }
       t.datetime :ends_at, { null: false }
       t.integer :lister_id, { null: false, index: true }
-      t.integer :item_id { null: false, index: true }
+      t.integer :item_id, { null: false, index: true }
 
       t.timestamps
     end
