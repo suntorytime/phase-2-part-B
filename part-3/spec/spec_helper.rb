61c92479c40dd6680355770a822b979887d3228a
Torey Hickman <torey@toreyhickman.com>
2015-04-10 10:16:54 -0400

    Configure spec_helper

--- part-3/spec/spec_helper.rb
+++ part-3/spec/spec_helper.rb
@@ -12,9 +12,26 @@ require 'shoulda-matchers'
 require 'rack/test'
 require 'capybara'
 require 'capybara/rspec'
+require 'database_cleaner'
+require 'factory_girl'
+require 'faker'
+
+path_to_factories = File.expand_path('../factories', __FILE__)
+Dir["#{path_to_factories}/*.rb"].each { |file| require file }
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
+
+  config.include FactoryGirl::Syntax::Methods
+
+  # DatabaseCleaner setup
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each)  { DatabaseCleaner.clean }
 end
 
 def app
