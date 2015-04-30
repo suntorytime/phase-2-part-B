6a1aa6a9f39c0675d90caef1e707e61063fb0500
Torey Hickman <torey@toreyhickman.com>
2015-04-30 18:13:23 -0500

    Update Item association specs

--- part-3/spec/models/item_spec.rb
+++ part-3/spec/models/item_spec.rb
@@ -2,14 +2,11 @@ require_relative "../spec_helper"
 
 describe Item do
   describe "model associations" do
-    before(:each) do
-      @guy = FactoryGirl.create(:lister)
-      @item = FactoryGirl.create(:item)
-      @auction = FactoryGirl.create(:auction, { lister: @guy, item: @item })
-    end
-
     it "returns the auction in which it's listed" do
-      expect(@item.auction).to eq @auction
+      trading_card_auction = FactoryGirl.build(:auction)
+      item = FactoryGirl.build(:item, { auction: trading_card_auction })
+
+      expect(item.auction).to eq trading_card_auction
     end
   end
 end
